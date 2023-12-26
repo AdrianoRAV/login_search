4578d303d96bbcb2aa1f447509d62587e68bc714
AdrianoRAV <adrianorodrigues38@gmail.com>
2023-12-26 10:16:00 -0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:login_search/page/home_page.dart';
+import 'package:login_search/page/login_page.dart';
+import 'package:login_search/page/teste_login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +20,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false,
-      home: const MyHomePage(title: 'Page'),
+      home: const LoginPage(),
     );
   }
 }
---
+++ lib/page/login_page.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({
+    super.key,
+  });
+
+  @override
+  State<LoginPage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 32,
+          vertical: 40,
+        ),
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
+          colors: [
+            Color.fromARGB(255, 40, 72, 98),
+            Colors.blueGrey,
+          ],
+        )),
+        child: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: Column(
+            children: [
+              //email
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Login',
+                ),
+              ),
+              SizedBox(height: 16.0),
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Senha',
+                ),
+              ),
+              SizedBox(height: 16.0),
+              GestureDetector(
+                onTap: () {
+                  // Adicione aqui a navegação para a página de política de privacidade
+                  // Exemplo: Navigator.push(context, MaterialPageRoute(builder: (context) => PrivacyPolicyPage()));
+                },
+                child: Text(
+                  'Política de Privacidade',
+                  style: TextStyle(
+                    color: Colors.blue,
+                    decoration: TextDecoration.underline,
+                  ),
+                ),
+              ),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () {
+                  // Adicione aqui a lógica de autenticação
+                },
+                child: Text('Entrar'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
